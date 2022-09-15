626548a14c5dddca616f5f04f9599381ede54868
adriamilan-cfgs <adria4milan@gmail.com>
2022-09-15 13:35:44 +0200

    RickMorty Crud

---
+++ RickMortyCrud/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterDetailsComponent } from './components/character-details/character-details.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterListComponent,
+    CharacterDetailsComponent,
+    AddCharacterComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ RickMortyCrud/src/app/components/character-details/character-details.component.ts
@@ -0,0 +1,105 @@
+import { registerLocaleData } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharactersService } from 'src/app/service/characters.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.css']
+})
+export class CharacterDetailsComponent implements OnInit {
+
+  editId: boolean = false;
+
+  public characterForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private chservice: CharactersService, private fb: FormBuilder, private router: Router) {
+    this.characterForm = this.fb.group({
+      name: '',
+      status: '',
+      species: '',
+      gender: '',
+      origin: '',
+      image: ''
+    })
+  }
+
+  idParam: any = null;
+
+  character: any;
+
+  ngOnInit(): void {
+    this.idParam = this.route.snapshot.paramMap.get('id');
+
+    this.chservice.getCharacter(this.idParam)
+      .subscribe(
+        data => {
+          this.character = data;
+          console.log(data)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+  }
+
+  deleteCharacter() {
+    this.chservice.delete(this.idParam)
+      .subscribe(
+        response => {
+          console.log(response)
+          this.router.navigate(['characters']);
+        },
+        error => {
+          console.log(error)
+        }
+      )
+  }
+
+  validateData() {
+    if (this.characterForm.get('name')?.value != null && this.characterForm.get('status')?.value != null && this.characterForm.get('species')?.value !=null && this.characterForm.get('gender')?.value != null && this.characterForm.get('origin')?.value != null && this.characterForm.get('image')?.value != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  editCharacter() {
+
+    const data = {
+      name: this.characterForm.get('name')?.value,
+      status: this.characterForm.get('status')?.value,
+      species: this.characterForm.get('species')?.value,
+      gender: this.characterForm.get('gender')?.value,
+      origin: this.characterForm.get('origin')?.value,
+      image: this.characterForm.get('image')?.value
+    }
+
+    let validator = this.validateData()
+
+    if (validator) {
+      this.chservice.updateCharacter(this.idParam, data)
+        .subscribe(
+          response => {
+            console.log(response)
+            window.location.reload();
+          },
+          error => {
+            console.log(error)
+          }
+        )
+    }
+  }
+
+  edit() {
+    if (this.editId) {
+      this.editId = false
+    } else {
+      this.editId = true
+    }
+  }
+
+}
